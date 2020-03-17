7f6668a4d65c1b17908f92cc1f44288cd0018146
brayn <voidpointers@hotmail.com>
2020-03-17 10:56:11 +0800

    add declares

--- database/migrations/2020_02_28_032326_create_shops_table.php
+++ database/migrations/2020_02_28_032326_create_shops_table.php
@@ -15,10 +15,10 @@ class CreateShopsTable extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->increments('id');
-            $table->bigInteger('user_id')->unsigned()->default(0)->comment('店铺ID');
-            $table->string('username')->default('')->comment('用户名');
-            $table->bigInteger('shop_id')->unsigned()->default(0)->comment('用户ID');
+            $table->bigInteger('shop_id')->unsigned()->default(0)->comment('店铺ID');
+            $table->bigInteger('user_id')->unsigned()->default(0)->comment('用户ID');
             $table->string('shop_name')->default('')->comment('店铺名');
+            $table->string('username')->default('')->comment('用户名');
             $table->string('title')->default('')->comment('店铺主页简短标题');
             $table->string('currency_code', 32)->default('')->comment('货币ISO代码');
             $table->string('shop_name_zh')->default('')->comment('店铺中文名');
@@ -30,6 +30,7 @@ class CreateShopsTable extends Migration
             $table->string('access_token')->default('')->comment('授权访问token');
             $table->string('access_secret')->default('')->comment('授权密钥');
             $table->tinyInteger('status')->unsigned()->default(0)->comment('状态');
+            $table->string('ip')->default('')->comment('店铺部署IP地址');
             $table->integer('create_time')->unsigned()->default(0)->comment('创建时间');
             $table->integer('update_time')->unsigned()->default(0)->comment('更新时间');
         });
---
+++ database/migrations/2020_03_17_024929_create_declares_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeclaresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('declares', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('shop_id')->unsigned()->default(0)->comment('店铺ID');
+            $table->decimal('price', 10, 2)->unsigned()->default(0)->comment('价格');
+            $table->float('weight', 10 ,2)->unsigned()->default(0)->comment('重量');
+            $table->tinyInteger('quantity')->unsigned()->default(0)->comment('数量');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('declares');
+    }
+}
